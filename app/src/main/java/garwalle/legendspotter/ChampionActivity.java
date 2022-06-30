d6e8da604b2b7952d8fea705e82f5ba06c1bddaa
MartinJeanne <martinjeanne.dev@gmail.com>
2022-06-30 22:38:28 +0200

    Can now switch between the differents champions skins !

--- app/src/main/java/garwalle/legendspotter/ChampionActivity.java
+++ app/src/main/java/garwalle/legendspotter/ChampionActivity.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,17 +19,23 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class ChampionActivity extends AppCompatActivity {
 
     TextView tvChampName;
     TextView tvChampSummary;
+    TextView tvNumSkin;
     ImageView ivChampImg;
     String champName;
+    ArrayList<Integer> skinsNumber = new ArrayList<Integer>();
+    int skinsIterator = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +44,54 @@ public class ChampionActivity extends AppCompatActivity {
 
         tvChampName = findViewById(R.id.tvChampName);
         tvChampSummary = findViewById(R.id.tvChampSummary);
+        tvNumSkin = findViewById(R.id.tvNumSkin);
+        tvNumSkin.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View view) { switchSkin(); }
+        });
+
         ivChampImg = findViewById(R.id.ivChampImg);
+        ivChampImg.setOnClickListener(new ImageView.OnClickListener() {
+            @Override
+            public void onClick(View view) { switchSkin(); }
+        });
 
         Intent intent = getIntent();
         champName = intent.getStringExtra("champName");
         tvChampName.setText(champName);
 
+        String champInfoUrl = "http://ddragon.leagueoflegends.com/cdn/12.12.1/data/fr_FR/champion/" + champName + ".json";
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET}, 1);
-            return;
-        }
+        JsonObjectRequest champInfo = new JsonObjectRequest(Request.Method.GET, champInfoUrl, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String lore = "Pas de description.";
+                try {
+                    lore = response.getJSONObject("data").getJSONObject(champName).getString("lore");
+                    JSONArray skins = response.getJSONObject("data").getJSONObject(champName).getJSONArray("skins");
+                    for (int i=0; i < skins.length(); i++) {
+                        int skin = skins.getJSONObject(i).getInt("num");
+                        skinsNumber.add(skin);
+                    }
+                } catch (JSONException e) {
+                    Log.v("--error", "Error : " + e);
+                }
+                tvChampSummary.setText(lore);
+                tvNumSkin.setText("Style : 1/" + skinsNumber.size());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-        String champImageUrl = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champName + "_0.jpg";
+            }
+        });
+
+        SingletonRequestQueue.getInstance(this).addToRequestQueue(champInfo);
+        loadImg(skinsIterator);
+    }
+
+    private void loadImg(int skinNumber) {
+        String champImageUrl = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champName + "_" + skinNumber + ".jpg";
 
         ImageRequest champImage = new ImageRequest(champImageUrl, new Response.Listener<Bitmap>() {
             @Override
@@ -63,29 +105,13 @@ public class ChampionActivity extends AppCompatActivity {
             }
         });
 
-
-        String champInfoUrl = "http://ddragon.leagueoflegends.com/cdn/12.12.1/data/fr_FR/champion/" + champName + ".json";
-
-        JsonObjectRequest champInfo = new JsonObjectRequest(Request.Method.GET, champInfoUrl, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                String lore = "Vide";
-                try {
-                     lore = response.getJSONObject("data").getJSONObject(champName).getString("lore");
-                    Log.v("--lore", lore);
-                } catch (JSONException e) {
-                    Log.v("--error", "Error : " + e);
-                }
-                tvChampSummary.setText(lore);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
-
         SingletonRequestQueue.getInstance(this).addToRequestQueue(champImage);
-        SingletonRequestQueue.getInstance(this).addToRequestQueue(champInfo);
+    }
+
+    private void switchSkin() {
+        if (skinsIterator + 1 < skinsNumber.size()) skinsIterator++;
+        else skinsIterator = 0;
+        loadImg(skinsNumber.get(skinsIterator));
+        tvNumSkin.setText("Style : " + (skinsIterator + 1) + "/" + skinsNumber.size());
     }
 }
